b193b5556e51017dbf4e65c39f3cf93b2848974d
Luciano Coscia <lucianocoscia5@gmail.com>
2022-08-29 23:48:36 -0300

    Agregando detalles Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,30 @@
 import logo from   '../images/logo12.png'
 import CartWidget from './CartWidget/CartWidget.js';
+import search from '../components/NavBar/search-icon.svg'
 
 const NavBar = () => {
     return (
         <header className='contenedor_navbar'>
             <nav className="navbar">
-                
+                <div className='navbar_buscador'>
+                    <input className='buscador_input' placeholder='Buscar...'></input>
+                    <img className='buscador_img' src={search}></img>
+                </div>
                 <img className="navbar_img" src={logo}  alt="logo"></img>
-                <ul className="navbar_ul">
-                    <li><a href="#">Inicio</a></li>
-                    <li><a href="#">Productos</a></li>
-                    <li><a href="#">Quienes Somos</a></li>
-                    <li><a href="#">Contacto</a></li>
+                <div className='contenedor_links' >
+                    <ul className="navbar_ul">
+                        <li><a href="#">Inicio</a></li>
+                        <li><a href="#">Productos</a></li>
+                        <li><a href="#">Quienes Somos</a></li>
+                        <li><a href="#">Contacto</a></li>
+                    </ul>
+                </div>
+                <div className='navbar_items'>
+                    <a className='link_registro'>Crear Cuenta</a>
+                    <a className='link_sesion'>Iniciar Sesión</a>
+                    <button className='button_Cart'><CartWidget/></button>
+                </div>
 
-                </ul>
-                <button className='button_Cart'><CartWidget/></button>
             </nav>
         </header>
     )}
